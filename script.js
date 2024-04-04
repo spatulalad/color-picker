dd47aed7a7dba23e8f651ee3f6d9d2aa0054c987
Mike Athey <mathey@cnm.edu>
2024-04-04 12:32:41 -0600

    finish color picker ii fixed blue value

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function initColorPicker(){
     let rgb = {
         red: document.getElementById("red"),
         green: document.getElementById("green"),
-        blue: document.getElementById("blue"),
+        blue: document.getElementById("blue")
     };
     let colorPickers = document.getElementsByClassName("picker");
     setColorPickerEventListeners(colorBox, rgb, colorPickers);
@@ -15,13 +15,13 @@ function initColorPicker(){
 
 function setColorPickerEventListeners(element, colors, pickerElements)  {
     let pickerLen = pickerElements.length;
-    for (let i = 0; i <pickerLen; i++) {
+    for (let i = 0; i < pickerLen; i++) {
         pickerElements[i].addEventListener('change', () => {
             let red = colors.red.value;
             let green = colors.green.value;
             let blue = colors.blue.value;
             setElementBGColor(element, red, green, blue);
-            setDisplayValues (red, green, blue)
+            setDisplayValues(red, green, blue)
         });
     }
 
@@ -35,11 +35,11 @@ function setElementBGColor (bgElement, red, green, blue) {
 function setDisplayValues(red, green, blue) {
     let redVal = document.getElementById("redVal");
     let greenVal = document.getElementById("greenVal");
-    let blueVal = document.getElementById("blueVal")
+    let blueVal = document.getElementById("blueVal");
 
     redVal.innerText = red;
     greenVal.innerText = green;
-    blueVal.innerext = blue;
+    blueVal.innerText = blue;
 }
 /*
     rgb.red.addEventListener('change', () => {
