673908d87f32a54a66037ece7a748a2f76058673
Mike Athey <mathey@cnm.edu>
2024-04-04 12:14:00 -0600

    finish color picker II blue not displaying number value

--- script.js
+++ script.js
@@ -9,17 +9,57 @@ function initColorPicker(){
         green: document.getElementById("green"),
         blue: document.getElementById("blue"),
     };
-    setColorPickerEventListeners(colorBox, rgb);
+    let colorPickers = document.getElementsByClassName("picker");
+    setColorPickerEventListeners(colorBox, rgb, colorPickers);
 }
 
-function setColorPickerEventListeners(colorBox, rgb) {
+function setColorPickerEventListeners(element, colors, pickerElements)  {
+    let pickerLen = pickerElements.length;
+    for (let i = 0; i <pickerLen; i++) {
+        pickerElements[i].addEventListener('change', () => {
+            let red = colors.red.value;
+            let green = colors.green.value;
+            let blue = colors.blue.value;
+            setElementBGColor(element, red, green, blue);
+            setDisplayValues (red, green, blue)
+        });
+    }
+
+}
+
+function setElementBGColor (bgElement, red, green, blue) {
+    let rgbVal = [red, green, blue].join(',');
+    bgElement.style.backgroundColor = "rgb(" + rgbVal + ")";
+}
+
+function setDisplayValues(red, green, blue) {
+    let redVal = document.getElementById("redVal");
+    let greenVal = document.getElementById("greenVal");
+    let blueVal = document.getElementById("blueVal")
+
+    redVal.innerText = red;
+    greenVal.innerText = green;
+    blueVal.innerext = blue;
+}
+/*
     rgb.red.addEventListener('change', () => {
         console.log("Red value: ", rgb.red.value);
+        setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
     });
     rgb.green.addEventListener('change',() => {
         console.log("Green value: ", rgb.green.value);
+        setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
     });
     rgb.blue.addEventListener('change', () => {
         console.log("Blue value: ", rgb.blue.value);
+        setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
     })
 }
+
+
+}
+
+//refactoring (rewriting code to be cleaner
+)
+ */
+
